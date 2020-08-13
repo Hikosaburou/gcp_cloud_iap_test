f4189887f2b90b7eb261bfe7129b5984571ee055
矢澤 学 <manabu.yazawa@manabu-yazawa1.local>
2020-08-13 11:34:32 +0900

    feat: firewallルール追加

--- gcp_terraform/gcp_network.tf
+++ gcp_terraform/gcp_network.tf
@@ -1,15 +1,16 @@
 resource "google_compute_network" "cloudiap_network" {
-  name                    = "${var.project-code}-network"
+  name                    = "${var.project_prefix}-network"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "cloudiap_subnetwork" {
-  name          = "${var.project-code}-subnetwork"
+  name          = "${var.project_prefix}-subnetwork"
   ip_cidr_range = "10.8.0.0/16"
-  region        = var.default-region
+  region        = var.default_region
   network       = google_compute_network.cloudiap_network.id
 }
 
+### Cloud IAP経由で gcloud ssh するための設定
 resource "google_compute_firewall" "cloudiap" {
   name    = "allow-ingress-from-iap"
   network = google_compute_network.cloudiap_network.name
@@ -20,5 +21,19 @@ resource "google_compute_firewall" "cloudiap" {
   }
 
   source_ranges = ["35.235.240.0/20"]
-  source_tags   = [var.tag-tunnel]
+  target_tags   = [var.tag_tunnel]
+}
+
+### グローバルHTTP(S)負荷分散からのアクセス許可設定
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http-access"
+  network = google_compute_network.cloudiap_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
+  target_tags   = [var.tag_allow_http]
 }
